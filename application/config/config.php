ec8712f42676bb7f56793a862c31ad49ca4ae5f8
Garin Septian <garin.septian@cbn.net.id>
2017-11-17 00:35:55 +0700

    [update] configuration

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://gsgar.in/project/glx/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
