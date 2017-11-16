8208141691709461c01c6c4121624b7e1dfda645
Garin Septian <garin.septian@cbn.net.id>
2017-11-17 00:36:55 +0700

    [update] added model for user login

---
+++ application/models/User_access.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+* @author: gsgarin
+*/
+class User_access extends CI_Model{
+	function cekLoginAdmin($data){
+		$query = $this->db->get_where('users', array('username' => $data['uname'],'password' => $data['passwd']), 1);
+        if($query->num_rows() == 1){
+            return 1;
+        }else{
+            return 0;
+        }
+	}
+	function getAdminInfo($data){
+        $query = $this->db->query("SELECT * FROM users WHERE username = '".$data['uname']."'");
+        return $query->row_array();
+    }
+    function daftarUser($data){
+        $this->db->insert('user', $data);
+    }
+} 
